8024f6d9eb85412da644778d7be805fb3f9eab26
BrokenShell <webmaster@sharpdesigndigital.com>
2020-05-07 16:30:58 -0700

    updates #17 deployment

---
+++ assignments/A17_deployment.py
@@ -0,0 +1,11 @@
+""" Assignment 17
+
+For this assignment you should design, build and deploy a Python3 package
+and answer the following:
+
+1. What changes do we need to make in the `setup.py` script in relation to
+installing a package vs a module?
+
+2. What is the difference between a python package and a module?
+
+"""
