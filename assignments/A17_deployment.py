01539a25a3efa75d5d8bb1e2293727a051a66904
BrokenShell <webmaster@sharpdesigndigital.com>
2020-05-13 12:56:29 -0700

    added assignments

--- assignments/A17_deployment.py
+++ assignments/A17_deployment.py
@@ -3,9 +3,9 @@
 For this assignment you should design, build and deploy a Python3 package
 and answer the following:
 
-1. What changes do we need to make in the `setup.py` script in relation to
-installing a package vs a module?
+1. What is the fundamental difference between a python package and a module?
 
-2. What is the difference between a python package and a module?
+2. What changes do we need to make in the `setup.py` script in relation to
+installing a package vs a module?
 
 """
